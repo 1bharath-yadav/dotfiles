dda38a6c8dc6878c1c48207965d2574c7eb649d7
1bharath-yadav <byadhav36@gmail.com>
2025-11-28 23:08:46 +0530

    chore(update): Improve update script robustness
    - Use `stow --restow` to ensure symlinks are correctly re-linked.
    - Simplify the log message.
    - Make the script executable.

--- update.sh
+++ update.sh
@@ -6,8 +6,8 @@ cd ~/.dotfiles/
 stow -D -v */
 cd ~/linux/end4dots && git stash && git pull && ./setup install
 cd ~/.dotfiles/
-stow -v */
+stow -v --restow */
 
-echo "$(date): Dotfiles update completed successfully" >> ~/.dotfiles/update.log
+echo "$(date)" >> ~/.dotfiles/update.log
 
 
