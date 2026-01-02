cad6b7c899da7b09be164cfa98c38df010124208
1bharath-yadav <byadhav36@gmail.com>
2026-01-02 14:17:06 +0530

    feat(config): Sync configs and update scripts
    - **Hyprland:**
      - Refactor window rules to the new `windowrule` syntax.
      - Remove scratchpad bindings for unused applications.
      - Adjust Obsidian scratchpad shortcut.
    
    - **illogical-impulse:**
      - Sync configuration with the latest version.
    
    - **Scripts:**
      - Remove `--adopt` from the `stow` command in `update.sh` for a safer update process.

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ cd ~/.dotfiles/
 stow -D -v */
 cd ~/linux/end4dots && git stash && git pull && ./setup install
 cd ~/.dotfiles/
-stow -v --adopt --restow */
+stow -v --restow */
 stow -D shell-sources
 echo "$(date)" >> ~/.dotfiles/update.log
 
