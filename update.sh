a4ca7e6296fb200df845551e5001ab7961d79420
1bharath-yadav <byadhav36@gmail.com>
2026-01-24 01:00:32 +0530

    refactor(hypr): Remove scratchpad keybinds and rules

--- update.sh
+++ update.sh
@@ -7,18 +7,32 @@ LOG="$DOTFILES/update.log"
 
 log() { echo "\e[32m==>\e[0m $1"; }
 
+# Stow package, auto-removing conflicts
+stow.it() {
+  local pkg="$1"
+  local conflicts=$(stow -nv "$pkg" 2>&1 | grep -oP '(?<=existing target is neither a link nor a directory: ).*')
+  [[ -n "$conflicts" ]] && echo "$conflicts" | xargs -I{} rm -rf ~/{} 
+  stow "$pkg"
+}
+
+# Restow package (unlink then relink), auto-removing conflicts
+restow() {
+  local pkg="$1"
+  stow -D "$pkg" 2>/dev/null || true
+  stow.it "$pkg"
+}
+
 cd "$DOTFILES"
-log "Unstowing packages..."
-stow -D */
 
 log "Updating end4dots..."
 cd "$END4DOTS" && git stash -q && git pull -q && ./setup install
 
-log "Cleaning conflicting configs..."
-rm -rf ~/.config/{hypr/custom,starship,kitty}
-
 log "Restowing packages..."
-cd "$DOTFILES" && stow --restow */ && stow -D shell-sources
+cd "$DOTFILES"
+for pkg in */; do
+  [[ "$pkg" == "shell-sources/" ]] && continue
+  restow "${pkg%/}"
+done
 
 echo "[$(date '+%F %T')] updated" >> "$LOG"
 log "Done!"
