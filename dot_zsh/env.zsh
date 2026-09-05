55bf417b818d682b6d8d0b28a52f833c4be2eb7c
1bharath-yadav <byadhav36@gmail.com>
2026-09-06 02:06:01 +0530

    chore(env): sync mise runtime toolchain, update zsh paths, and remove pacman package manifest

--- dot_zsh/env.zsh
+++ dot_zsh/env.zsh
@@ -3,7 +3,7 @@ export EDITOR="nvim"
 export SUDO_EDITOR="nvim"
 export MANPAGER="nvim +Man!"
 export PNPM_HOME="$HOME/.local/share/pnpm"
-
+export PATH="$HOME/.local/share/mise/shims:$PATH"
 path=(
   "$HOME/.local/bin"
   "$HOME/.local/share/pnpm"
